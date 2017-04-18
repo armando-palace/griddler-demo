352670467cae0c118378793c72bdade6f06554ec
Jesús <iijbari@gmail.com>
2017-04-18 13:11:43 -0400

    Edited .gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.default_url_options = { host: "localhost:3000" }
+  # Don't care if the mailer can't send.
+ActionMailer::Base.raise_delivery_errors = true
+
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
+  #:user_name => "jesusdibyte",
+  #:password => "Sailll77",
   :domain => 'gmail.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
