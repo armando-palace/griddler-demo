63c86b89d95de882e79a22b13804b65d3db3433a
Jesús <iijbari@gmail.com>
2017-04-18 17:39:25 -0400

    Environment variables added successfully

--- config/environment.rb
+++ config/environment.rb
@@ -8,13 +8,23 @@ ActionMailer::Base.default_url_options = { host: "localhost:3000" }
   # Don't care if the mailer can't send.
 ActionMailer::Base.raise_delivery_errors = true
 
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => ENV["SENDGRID_USERNAME"],
+#   :password => ENV["SENDGRID_PASSWORD"],
+#   #:user_name => "jesusdibyte",
+#   #:password => "Sailll77",
+#   :domain => 'gmail.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
+
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_PASSWORD"],
-  #:user_name => "jesusdibyte",
-  #:password => "Sailll77",
+  :user_name => ENV["GMAIL_USERNAME"],
+  :password => ENV["GMAIL_PASSWORD"],
   :domain => 'gmail.com',
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,21 +60,21 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  # config.action_mailer.default_url_options = { host: "localhost:3000" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: "gmail.com",
-    authentication: :plain,
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.sendgrid.net",
+  #   port: 587,
+  #   domain: "gmail.com",
+  #   authentication: :plain,
+  #   :user_name => ENV["SENDGRID_USERNAME"],
+  #   :password => ENV["SENDGRID_PASSWORD"],
+  #   enable_starttls_auto: true
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
