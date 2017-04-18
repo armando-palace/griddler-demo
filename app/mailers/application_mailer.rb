ef45e1d969ac9d681c42d75fe508a81a3eb65dc7
Jesús <iijbari@gmail.com>
2017-04-18 11:51:14 -0400

    Initialize repository

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "iijbari@gmail.com"
+  layout 'mailer'
+end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,19 @@
+class UserMailer < ApplicationMailer
+  include SendGrid
+  sendgrid_category :use_subject_lines
+  sendgrid_enable   :ganalytics, :opentrack
+  sendgrid_unique_args :key1 => "value1", :key2 => "value2"
+
+  def welcome_email(user)
+    @user = user
+    @url  = "https://ruby-on-rails-armando-palace.c9users.io"
+    sendgrid_category "Welcome"
+    sendgrid_unique_args :key2 => "newvalue2", :key3 => "value3"
+    mail :to => @user.email, :subject => "Welcome #{@user.name} :-)"
+  end
+
+  def goodbye_message(user)
+    sendgrid_disable :ganalytics
+    mail :to => user.email, :subject => "Fare thee well :-("
+  end
+end
